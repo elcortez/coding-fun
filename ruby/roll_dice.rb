db7137fb7dae27b1fae1b89ba8b228f4691a6473
Pierre Hersant <phersant@protonmail.com>
2017-11-19 15:24:14 +0100

    Tracking Hits for Different Sum Values for Different Kinds of Dice

--- ruby/roll_dice.rb
+++ ruby/roll_dice.rb
@@ -1,3 +1,13 @@
+def reg_sum_hits(number_of_dice, sides)
+  (1..sides).to_a.repeated_permutation(number_of_dice).to_a
+    .map{|a| a.reduce(:+)}
+    .sort.inject({}) do |collection, hits|
+      collection[hits] ? collection[hits] += 1 : collection[hits] = 1
+      collection
+    end
+    .to_a
+end
+
 def rolldice_sum_prob(sum, number_of_dice)
   (1..6).to_a.repeated_permutation(number_of_dice).to_a
     .reject { |combination| combination.reduce(:+) != sum }
